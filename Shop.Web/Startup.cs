56f1ce1df1c18df95907789c69098bde27a9c445
Alexander Cubillos Hernandez <jaircubillosh@outlook.com>
2020-05-08 22:54:07 -0500

    sdfg

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -23,7 +23,7 @@ namespace Shop.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(cfg =>
+            services.AddDbContext<DataContext>(cfg => 
             {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
